3c3849431e6bb42c6fd07a78eacde8b2dd643618
Marc <marc.bernalo@e-campus.uab.cat>
2020-02-26 19:37:47 +0100

    Upload Photo

--- views/PaginaPrincipal.php
+++ views/PaginaPrincipal.php
@@ -6,29 +6,8 @@
  }?>
     <section id="features" class="features sections">
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" >
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">Important!</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Aquest portal es troba sota desenvolupament. Preguem que, si trobeu cap error, disculpeu les molèsties.
+        <!-- TODO: COPIAR Modal aquí -->
 
-                        En cas que l'error persisteixi agrairíem que contactéssiu amb l'administrador.
-
-                        Gràcies
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">D'acord</button>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <div class="col-sm-12">
             <div class="panel panel-info panel-custom">
